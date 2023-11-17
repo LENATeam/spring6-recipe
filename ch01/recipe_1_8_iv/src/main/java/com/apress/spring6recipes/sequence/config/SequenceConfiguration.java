6a583b7e809cebae432918a4328e565c88c84718
seosej <seosej@gmail.com>
2023-11-18 01:19:43 +0900

    1장 8절 소스코드 커밋

---
+++ ch01/recipe_1_8_iv/src/main/java/com/apress/spring6recipes/sequence/config/SequenceConfiguration.java
@@ -0,0 +1,23 @@
+package com.apress.spring6recipes.sequence.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import com.apress.spring6recipes.sequence.DatePrefixGenerator;
+import com.apress.spring6recipes.sequence.Sequence;
+
+@Configuration
+public class SequenceConfiguration {
+
+	@Bean
+	@DependsOn("datePrefixGenerator")
+	public Sequence sequenceGenerator() {
+		return new Sequence("A", 100000);
+	}
+
+	@Bean
+	public DatePrefixGenerator datePrefixGenerator(){
+		return new DatePrefixGenerator();
+	}
+}
