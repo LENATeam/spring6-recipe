49dc5ff84edfe47c50549efa47acb2633d2bba01
유기연 책임/시스템솔루션사업팀 <ygygood@lgcns.com>
2023-11-27 23:38:56 +0900

    feat : add ch02_01~06 source

---
+++ ch02/recipe_2_2_ii/src/main/java/com/apress/spring6recipes/court/service/InMemoryMemberService.java
@@ -0,0 +1,30 @@
+package com.apress.spring6recipes.court.service;
+
+import com.apress.spring6recipes.court.domain.Member;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+class InMemoryMemberService implements MemberService {
+
+	private Map<String, Member> members = new HashMap<>();
+
+	public void add(Member member) {
+		members.put(member.getName(), member);
+	}
+
+	public void remove(String memberName) {
+		members.remove(memberName);
+	}
+
+	@Override
+	public Optional<Member> find(String memberName) {
+		return Optional.empty();
+	}
+
+	public List<Member> list() {
+		return new ArrayList<>(members.values());
+	}
+
+}
